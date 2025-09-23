f86ddc8889f59b732161829c385ecf81f115fab6
alcacere <alcacere@localhost.com>
2025-09-23 23:07:36 +0200

    gnl correction of mem leak in edge case

--- tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $# -eq 0 ]; then
-	./pipex Makefile "cat -e" "cat -e" "cat -e" "cat -e" outfile
-fi
-if [ -n "$1" ]; then
-	./pipex heredoc del "cat -e" "cat -e" "cat -e" outfile
-fi
